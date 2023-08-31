2e8aef86e27c00cfc905d67c542e2f577fafea6b
daniel-su1 <107098486+daniel-su1@users.noreply.github.com>
2023-08-31 16:30:18 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((prevItems) => [...prevItems, item]);
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems}/>
+      <PackingList items={items}/>
       <Stats />
     </div>
   );
@@ -20,9 +24,12 @@ function Logo() {
   return <h1>🌴Far Away💼</h1>;
 }
 
-function Form() {
+function Form({onAddItems}) {
   const [description, setDescription] = useState('');
   const [quantity, setQuantity] = useState(1);
+  
+
+  
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -30,6 +37,8 @@ function Form() {
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
+
+    onAddItems(newItem);
 
     setDescription('');
     setQuantity(1);
@@ -50,11 +59,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id}/>
         ))}
       </ul>
